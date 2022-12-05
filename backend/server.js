f6d6cf5867e5c4d9167e23e550499070dff2f45a
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-05 13:13:44 +0100

    Connected Database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // express app
@@ -17,8 +18,17 @@ app.use((req, res, next) => {
 // routes
 app.use('/api/workouts', workoutRoutes)
 
-// listen for requests
-const port = process.env.PORT
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connect to database')
+        // listen for requests
+        const port = process.env.PORT
+        app.listen(port, () => {
+            console.log(`Listening on port ${port}`)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
