8c6eadff9fbe0da62bab4cf7a1443efa56dceb0c
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-12 13:33:38 +0100

    Created workout model

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 
 app.use((req, res, next) => {
-    console.log('Test', req.path, req.method)
+    console.log(req.path, req.method)
     next()
 })
 
