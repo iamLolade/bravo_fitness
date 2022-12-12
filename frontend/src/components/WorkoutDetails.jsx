589a5264a1eead21d150d2e13b1963f5f2677e5a
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-12 18:49:25 +0100

    Created and styled workout details component

---
+++ frontend/src/components/WorkoutDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function WorkoutDetails({ workout }) {
+  return (
+    <div className='workout-details'>
+        <h4>{workout.title}</h4>
+        <p><strong>Load (kg): </strong>{workout.load}</p>
+        <p><strong>Number of reps: </strong>{workout.reps}</p>
+        <p>{workout.createdAt}</p>
+    </div>
+  )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
 
 export default function Home() {
-  return (
-    <div className='home'>
-        <h2>Home</h2>
-    </div>
-  )
+    const [workouts, setWorkouts] = useState(null)
+    console.log(workouts)
+
+    useEffect(() => {
+        const fetchWorkouts = async () => {
+            const response = await fetch('/api/workouts')
+            const data = await response.json()
+
+            if(response.ok) {
+                setWorkouts(data)
+            }
+        }
+
+        fetchWorkouts();
+    }, [])
+
+    return (
+        <div className='home'>
+            <div className='workouts'>
+                {workouts && workouts.map(workout => (
+                    <WorkoutDetails key={workout._id} workout={workout} />
+                ))}
+            </div>
+        </div>
+    )
 }
