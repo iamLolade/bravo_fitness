9ffa547418b830094e2f89778129c2deeeded12f
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-18 21:03:18 +0100

    Created User routes

---
+++ backend/routes/user.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+// login route
+router.post('/login', () => {})
+
+// register route
+router.post('/register', () => {})
+
+module.exports = router
