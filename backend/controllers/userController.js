244ecd4281041c8f02d14393eff2777ce0dafc8c
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-19 17:16:44 +0100

    Validated email and password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,19 @@
+const User = require('../models/userModel')
+
 const loginUser = async (req, res) => {
     res.json({ msg: 'User logged in'})
 }
 
 const registerUser = async (req, res) => {
+    const { email, password } = req.body
+
+    try {
+        const user = await User.register(email, password)
+        res.status(200).json({ email, user })
+    } catch (error) {
+        res.status(400).json({ error: error.message })
+    }
+
     res.json({ msg: 'User registered'})
 }
 
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const validator = require('validator')
 
 const Schema = mongoose.Schema
 
@@ -13,5 +15,35 @@ const userSchema = new Schema({
         required: true
     }
 })
+
+// static register method
+
+userSchema.statics.register = async function(email, password) {
+
+    if(!email || !password) {
+        throw Error('All fields must be filled!')
+    }
+
+    if(!validator.isEmail(email)) {
+        throw Error('Invalid email')
+    }
+
+    if(!validator.isStrongPassword(password)) {
+        throw Error('Password not strong enough')
+    }
+
+    const exist = await this.findOne({ email })
+
+    if(exist) {
+        throw Error('Email already exists')
+    }
+
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+
+    const user = this.create({ email, password: hash})
+
+    return user
+}
 
 module.exports = mongoose.model('User', userSchema)
