ed080601506b2f5b981ab1087aff8e2eda1e5526
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-20 20:54:47 +0100

    User check via localStorage

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Navbar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
 
   const handleLogout = () => {
     logout()
   }
-  
+
   return (
     <header>
         <div className='nav'>
@@ -16,13 +18,18 @@ export default function Navbar() {
                 <h1>Bravo Fitness</h1>
             </Link>
             <nav>
-              <div>
-                <button onClick={handleLogout}>Log out</button>
-              </div>
-              <div>
-                <Link to='/login'>Login</Link>
-                <Link to='/register'>Register</Link>
-              </div>
+              {user && (
+                <div>
+                  <span>{user.email}</span>
+                  <button onClick={handleLogout}>Log out</button>
+                </div>
+              )}
+              {!user && (
+                <div>
+                  <Link to='/login'>Login</Link>
+                  <Link to='/register'>Register</Link>
+                </div>
+              )}
             </nav>
         </div>
     </header>
