a2bb5eca825a3d3cbae0ceebd4b0ad1dfdd5bd7a
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-20 21:05:56 +0100

    Routes protected

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -9,6 +9,8 @@ const {
 const requireAuth = require('../middleware/requireAuth') 
 
 const router = express.Router()
+
+// require auth for all workout routes
 router.use(requireAuth)
 
 // GET all workouts
