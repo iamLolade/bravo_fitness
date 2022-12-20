ccc37c77547bc1224a85124e89d6cd635d8aaa10
Ololade Otemade <adeniyiotemade@gmail.com>
2022-12-20 17:25:50 +0100

    Created Login page

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+
+export default function Login() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        console.log(email, password)
+    }
+    return (
+        <form className='login' onSubmit={handleSubmit}>
+            <h3>Log In</h3>
+
+            <label>Email address: </label>
+            <input
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+            />
+            <input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={email}
+            />
+
+            <button>Log in</button>
+        </form>
+    )
+}
